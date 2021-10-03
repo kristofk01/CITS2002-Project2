255f6be66865854b03fee0538cf46d370d5b7cf2
Kristof Kovacs <23499974+kristofk01@users.noreply.github.com>
2021-10-03 10:37:47 +0800

    types

--- duplicates.c
+++
@@ -1,8 +0,0 @@
-//  CITS2002 Project 2 2021
-//  Name(s):             Kristof Kovacs , Daniel Ling
-//  Student number(s):   22869854       , 22896002
-
-int main(int argc, char *argv[])
-{
-    return 0;
-}
