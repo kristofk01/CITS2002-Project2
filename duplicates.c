393836e8f1712b5c2da5170b70eeefa755515b5c
Kristof Kovacs <23499974+kristofk01@users.noreply.github.com>
2021-10-03 15:09:24 +0800

    usage

--- duplicates.c
+++ duplicates.c
@@ -1,32 +1,33 @@
 //  CITS2002 Project 2 2021
 //  Name(s):             Kristof Kovacs , Daniel Ling
 //  Student number(s):   22869854       , 22896002
+
+// TODO: at the end, figure out what header files are needed for most .c files
+// and move them to duplicates.h
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
+
 #include "duplicates.h"
 
-#define OPLIST "aAf:h:lq"
+#define OPLIST "aAmf:h:lq"
+
+void usage(char *program_name)
+{
+    // TODO: remove m flag if we don't actually end up implementing it
+    printf("Usage: %s [-aAm] [-l | -q] [-f filename] [-h hash] <directory...>\n",
+            program_name);
+}
 
 int main(int argc, char *argv[])
 {
+    char *program_name = argv[0];
+
     int opt;
-    char *fname = NULL;
+    D_FILE arg_file;
 
-    //operr = 0;
-    if(opt == 'a')
-    {
-
-    }
-    else if(opt == 'A')
-    {
-
-    }
-    else if(opt == 'f')
-    {
-        //fname = strdup();
-    }
-    else if(opt == 'h')
-    {
-
-    }
+    if(argc <= 0) usage(program_name);
 
     exit(EXIT_SUCCESS);
 }
